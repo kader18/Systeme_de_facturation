f40dda40b2fe1a4aafb17d4ac008bf5acf784aa0
Sorokader <kadersoro18@gmail.com>
2024-03-20 17:12:48 +0000

    Adding existing code

---
+++ fact_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import *
+
+
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'address', 'sex', 'age', 'city', 'zip_code')
+
+class AdminInvoice(admin.ModelAdmin):
+    list_display=('customer','save_by','invoice_date_time','total','paid','last_updated_date','invoice_type')
+
+admin.site.register(Customer, AdminCustomer)
+admin.site.register(Invoice, AdminInvoice)
+admin.site.register(Article)
